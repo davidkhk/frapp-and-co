d64ac286a642e90bb69bc35e242a36b81e6a9da6
David <davidkhk@gmail.com>
2021-12-06 18:07:13 -0300

    Fix Instagram pictures

--- src/components/sections/instagram.jsx
+++ src/components/sections/instagram.jsx
@@ -4,8 +4,9 @@ import Mocha from '../../images/mocha.jpg';
 import CoffeeBoomerang from '../../images/coffee-boomerang.gif';
 import Cake from '../../images/cake.jpeg';
 import CoffeeBag from '../../images/coffeebag.jpeg';
-import SaltedCaramel from '../../images/salted-caramel.jpeg';
+import SaltedCaramel from '../../images/salted-caramel.jpg';
 import StrawberryBanana from '../../images/strawberry-banana.jpeg';
+import { FaInstagram } from 'react-icons/fa';
 
 const Section = styled.div`
     display: flex;
@@ -20,21 +21,38 @@ const Container = styled.div`
     margin-inline: 10%;
 `
 const InstaImg = styled.a`
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 300px;
+    height: 300px;
+    border: 5px var(--clr-primary1) solid;
+
     img {
         object-fit: cover;
-        width: 300px;
-        height: 300px;
-        border: 5px var(--clr-primary1) solid;
-        opacity: 1;
-        transition: ease-out .2s;
+        width: 100%;
+        height: 100%;
     }
-    video {
-        width: 300px;
-        height: 300px;
-        border: 5px var(--clr-primary1) solid;
+    div {
+        padding: 120px;
+        color: var(--clr-primary2);
+        font-size: 3rem;
+        position: absolute;
+        opacity: 0;
+    }
+
+    &:hover {
+        img {
+            transition: all .2s ease-in;
+            opacity: .3;
+        }
+        div {
+            transition: all .2s ease-in;
+            opacity: 1;
+        }
     }
 `
+
 const InstaLink = styled.a`
   font-size: 1.2rem;
   cursor: pointer;
@@ -57,12 +75,42 @@ function Instagram() {
         <Section id="instagram">
             <h1>Segue a gente no Instagram!</h1>
             <Container>
-                <InstaImg href="https://www.instagram.com/p/CTzU4t8lzxz/" target="_blank" rel='noreferrer'><img src={SaltedCaramel} alt="Frapp salted caramel" /></InstaImg>
-                <InstaImg href="https://www.instagram.com/p/CUfv9ETlBDe/" target="_blank" rel='noreferrer'><img src={Cake} alt="Carrot cake" /></InstaImg>
-                <InstaImg href="https://www.instagram.com/p/CWOMimghDl6/" target="_blank" rel='noreferrer'><img src={CoffeeBoomerang} alt="Coffee bag boomerang" /></InstaImg>
-                <InstaImg href="https://www.instagram.com/p/CUX7v1XlA9s/" target="_blank" rel='noreferrer'><img src={StrawberryBanana} alt="Strawberry banana frapp" /></InstaImg>
-                <InstaImg href="https://www.instagram.com/p/CQhYrWfj5d-/" target="_blank" rel='noreferrer'><img src={Mocha} alt="Mocha" /></InstaImg>
-                <InstaImg href="https://www.instagram.com/p/CVJmn5il6iX/" target="_blank" rel='noreferrer'><img src={CoffeeBag} alt="Coffee bag" /></InstaImg>
+                <InstaImg href="https://www.instagram.com/p/CTzU4t8lzxz/" target="_blank" rel='noreferrer'>
+                    <img src={SaltedCaramel} alt="Frapp salted caramel" />
+                    <div>
+                        <FaInstagram/>
+                    </div>
+                </InstaImg>
+                <InstaImg href="https://www.instagram.com/p/CUfv9ETlBDe/" target="_blank" rel='noreferrer'>
+                    <img src={Cake} alt="Carrot cake" />
+                    <div>
+                        <FaInstagram/>
+                    </div>
+                </InstaImg>
+                <InstaImg href="https://www.instagram.com/p/CWOMimghDl6/" target="_blank" rel='noreferrer'>
+                    <img src={CoffeeBoomerang} alt="Coffee bag boomerang" />
+                    <div>
+                        <FaInstagram/>
+                    </div>
+                </InstaImg>
+                <InstaImg href="https://www.instagram.com/p/CUX7v1XlA9s/" target="_blank" rel='noreferrer'>
+                    <img src={StrawberryBanana} alt="Strawberry banana frapp" />
+                    <div>
+                        <FaInstagram/>
+                    </div>
+                </InstaImg>
+                <InstaImg href="https://www.instagram.com/p/CQhYrWfj5d-/" target="_blank" rel='noreferrer'>
+                    <img src={Mocha} alt="Mocha" />
+                    <div>
+                        <FaInstagram/>
+                    </div>
+                </InstaImg>
+                <InstaImg href="https://www.instagram.com/p/CVJmn5il6iX/" target="_blank" rel='noreferrer'>
+                    <img src={CoffeeBag} alt="Coffee bag" />
+                    <div>
+                        <FaInstagram/>
+                    </div>
+                </InstaImg>
             </Container>
             <InstaLink href="https://www.instagram.com/frapp.and.co/" target="_blank" rel="noreferrer">@frapp.and.co</InstaLink>
         </Section>
