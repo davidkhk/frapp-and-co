754caf10caae8c5c31740508e8bb3a0824003e77
David Kang <davidkhk@gmail.com>
2022-07-11 13:15:34 -0300

    fix: colons and minor fixes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { Navbar, Home, About, Shop, Delivery, Instagram, Contact, Footer } from './components'
+import {
+  Navbar,
+  Home,
+  About,
+  Shop,
+  Delivery,
+  Instagram,
+  Contact,
+  Footer,
+} from './components';
 
 const App = () => {
-    return (
-        <>
-            <Navbar />
-            <Home />
-            <About />
-            <Shop />
-            <Delivery />
-            <Instagram />
-            <Contact />
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Shop />
+      <Delivery />
+      <Instagram />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import reactDOM from 'react-dom';
 import App from './App';
-import GlobalStyles from './styles/globalStyle'
+import GlobalStyles from './styles/globalStyle';
 
 reactDOM.render(
-    <React.StrictMode>
-        <GlobalStyles />
-        <App />
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
   </React.StrictMode>,
-document.getElementById('root'));
+  document.getElementById('root')
+);
